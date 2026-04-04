6be18fc9d37ef1528a1c5c004ac1a66621b61d9e
Aayush Bista <aayushbista70@gmail.com>
2026-04-04 23:01:20 +0545

    Updated App.jsx

--- recipe-meal-planner/src/App.jsx
+++ recipe-meal-planner/src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import RecipeList from './components/RecipeList';
+import MealPlanner from './components/MealPlanner';
 import Footer from './components/Footer';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Hero />
       <RecipeList />
+      <MealPlanner />
       <Footer />
     </div>
   );
